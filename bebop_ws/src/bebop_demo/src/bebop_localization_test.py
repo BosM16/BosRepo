2fffcffe793201a7201e5a60964fc65f6763df8f
BosMathias <bos.mathias16@gmail.com>
2018-10-09 17:40:00 +0200

    Clean up for test of tryad_openvr.

--- bebop_ws/src/bebop_demo/src/bebop_localization_test.py
+++ bebop_ws/src/bebop_demo/src/bebop_localization_test.py
@@ -19,43 +19,14 @@ class LocalizationTest(object):
         '''
         rospy.init_node('bebop_demo')
 
-        self.pos_update = rospy.Publisher('pose_est', Pose2D, queue_size=1)
-        rospy.Subscriber('bebop/cmd_vel', Twist, self.kalman_pos_predict)
-        rospy.Subscriber('twist1_pub_', Twist, self.kalman_pos_correct)
+        self.pos_update = rospy.Publisher(
+            'pose_est', PoseStamped, queue_size=1)
 
     def start(self):
         '''
         Starts running of bebop_demo node.
         '''
         rospy.spin()
-
-    def kalman_pos_predict(self, data):
-        '''
-        Based on the velocity commands send out by the velocity controller,
-        calculate a prediction of the position in the future.
-        '''
-        self.wm.predict_pos_update(data)
-
-        pose_est = Pose2D()
-        pose_est.x = self.wm.xhat.x
-        pose_est.x = self.wm.xhat.x
-        self.pos_update.publish(pose_est)
-
-    def kalman_pos_correct(self, data):
-        # timing data to know length of preceding prediction step necessary
-        '''
-        Whenever a new position measurement is available, sends this
-        information to the perception and then triggers the kalman filter
-        to apply a correction step.
-        '''
-        self.pc.pose_vive = data
-        # data moet nog verwerkt worden naar gewenste formaat
-
-        pos_output = Pose2D
-        pos_output.x = self.pc.pose_vive.linear.x
-        pos_output.y = self.pc.pose_vive.linear.y
-        self.wm.correct_pos_update(pos_output)
-        self.pc.new_val = False
 
 
 if __name__ == '__main__':
