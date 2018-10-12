a0014e277fdee6ba70a79ee40c43d2af148373b3
BosMathias <bos.mathias16@gmail.com>
2018-10-12 13:01:31 +0200

    Working on calibration setup.

--- bebop_ws/src/bebop_demo/src/bebop_localization_test.py
+++ bebop_ws/src/bebop_demo/src/bebop_localization_test.py
@@ -2,10 +2,11 @@
 
 from geometry_msgs.msg import Twist
 from std_msgs.msg import Empty
+import numpy as np
 import rospy
+import sys
 import triad_openvr
 import time
-import sys
 
 
 class LocalizationTest(object):
@@ -17,66 +18,116 @@ class LocalizationTest(object):
         '''
         Initialization of Demo object.
         '''
+        self.tracked_object = 'tracker'
 
-        self.pub_rate = 60.0  # Hz
+        self.pose_est_world = Twist()
+        self.T_w_to_h = np.identity(4)
+        self.T_h_to_w = np.identity(4)
 
-        self.pos_update = rospy.Publisher(
-            'pose_est', Twist, queue_size=1)
+        self.pos_update = rospy.Publisher('pose_est', Twist, queue_size=1)
 
         rospy.Subscriber('publish_poses', Empty, self.publish_pose_est)
+        rospy.Subscriber('calibrate', Empty, self.calibrate)
 
     def start(self):
         '''
         Starts running of bebop_demo node.
         '''
         rospy.init_node('bebop_demo')
-        print 'loc test is ON'
+        print 'Lcalization test is ON'
+        self.v = triad_openvr.triad_openvr()
+        self.v.print_discovered_objects()
         rospy.spin()
 
-    def publish_pose_est(self, ignore):
-        print 'in publish pose est'
-        v = triad_openvr.triad_openvr()
-        v.print_discovered_objects()
-        print 'printed discovered objects'
+    def publish_pose_est(self, *_):
+        # print 'in publish pose est'
+        (pose_est_vive, pose_vive) = self.get_pose_est()
 
-        # if len(sys.argv) == 1:
-        interval = 1/self.pub_rate
-        # elif len(sys.argv) == 2:
-        #     interval = 1/float(sys.argv[0])
-        # else:
-        #     print("Invalid number of arguments")
-        #     interval = False
+        pos_vive = np.append(pose_vive[0:3], 1.)
 
-        print 'interval is set'
-        print interval
-        if interval:
-            print 'in if'
-            while(True):
-                print 'in while'
-                start = time.time()
-                # txt = ""
-                # for each in v.devices["tracker_1"].get_pose_euler():
-                #     txt += "%.4f" % each
-                #     txt += " "
-                # print("\r" + txt, end="")
+        print '--------------------------- \n'
+        print 'new position estimate \n'
+        print 'pos_vive', pos_vive
+        pos_world = np.matmul(self.T_h_to_w, pos_vive)
+        print 'pos world', pos_world
+        self.pose_est_world.linear.x = pos_world[0]
+        self.pose_est_world.linear.y = pos_world[1]
+        self.pose_est_world.linear.z = pos_world[2]
+        # print 'publish'
+        #  print self.pose_est_world
+        self.pos_update.publish(self.pose_est_world)
 
-                # Bos: make a Pose out of the read list and publish it.
-                pose = v.devices["tracker_1"].get_pose_euler()
-                pose_est = Twist()
-                pose_est.linear.x = pose[0]
-                pose_est.linear.y = pose[1]
-                pose_est.linear.z = pose[2]
+    def calibrate(self, *_):
+        pose_est, pose = self.get_pose_est()
 
-                pose_est.angular.x = pose[5]
-                pose_est.angular.y = pose[4]
-                pose_est.angular.z = pose[3]
+        transl_w_in_h = pose[0:3]
+        print 'translation \n', transl_w_in_h
+        roll = pose_est.angular.x
+        pitch = pose_est.angular.y
+        yaw = pose_est.angular.z
 
-                print 'publish'
-                self.pos_update.publish(pose_est)
+        R_yaw = np.array([[np.cos(yaw), -np.sin(yaw), 0.],
+                          [np.sin(yaw),  np.cos(yaw), 0.],
+                          [0.,            0.,           1.]])
 
-                sleep_time = interval-(time.time()-start)
-                if sleep_time > 0:
-                    time.sleep(sleep_time)
+        R_pitch = np.array([[np.cos(pitch),  0., np.sin(pitch)],
+                            [0.,             1.,            0.],
+                            [-np.sin(pitch), 0., np.cos(pitch)]])
+
+        R_roll = np.array([[1.,           0.,            0.],
+                           [0., np.cos(roll), -np.sin(roll)],
+                           [0., np.sin(roll),  np.cos(roll)]])
+
+        print 'Rotation matrices \n'
+        print 'roll \n', R_roll
+        print 'pitch \n', R_pitch
+        print 'yaw \n', R_yaw
+
+        R_tracker_in_vive = np.matmul(R_yaw, np.matmul(R_pitch, R_roll))
+        # Tracker z-axis points down. Add extra rotation 180 deg roll.
+        R_world_in_tracker = np.array([[1.,  0.,  0.],
+                                       [0., -1.,  0.],
+                                       [0.,  0., -1.]])
+        R_tracker_in_vive = np.matmul(R_tracker_in_vive, R_world_in_tracker)
+
+        print 'rotation matrix \n', R_tracker_in_vive
+
+        self.T_w_to_h[0:3, 3] = transl_w_in_h
+        self.T_w_to_h[0:3, 0:3] = R_tracker_in_vive
+
+        R_vive_in_tracker = np.transpose(R_tracker_in_vive)
+        print 'rotation matrix inverse \n', R_vive_in_tracker
+        self.T_h_to_w[0:3, 3] = - np.matmul(R_vive_in_tracker, transl_w_in_h)
+        self.T_h_to_w[0:3, 0:3] = R_vive_in_tracker
+
+        print 'T world to htc \n', self.T_w_to_h
+        print 'T htc to world \n', self.T_h_to_w
+        print 'T world to htc inverted \n', np.linalg.inv(self.T_w_to_h)
+
+    def get_pose_est(self):
+        # controller_1 must become tracker_1 when working with the tracker.
+        if self.tracked_object is 'controller':
+            pose = np.array(self.v.devices["controller_1"].get_pose_euler())
+        elif self.tracked_object is 'tracker':
+            pose = np.array(self.v.devices["tracker_1"].get_pose_euler())
+
+        else:
+            print 'No device found.'
+            return
+        # print 'pose deg \n', pose
+        pose[3:6] = pose[3:6]*np.pi/180.
+
+        pose_est = Twist()
+        pose_est.linear.x = pose[0]
+        pose_est.linear.y = pose[1]
+        pose_est.linear.z = pose[2]
+
+        pose_est.angular.x = pose[5]
+        pose_est.angular.y = pose[4]
+        pose_est.angular.z = pose[3]
+
+        # print 'pose rad \n', pose
+        return (pose_est, pose)
 
 
 if __name__ == '__main__':
