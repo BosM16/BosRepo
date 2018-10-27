9417a045c6286da42212af7f4deaf678329c2042
RianBeck <36485849+RianBeck@users.noreply.github.com>
2018-10-27 18:22:40 +0200

    update self.wm.X when measurement to time t0

--- bebop_ws/src/bebop_demo/src/bebop_test.py
+++ bebop_ws/src/bebop_demo/src/bebop_test.py
@@ -100,9 +100,10 @@ class Demo(object):
         '''
         if self.init:
             self.wm.xhat_r_t0.point = measurement_world.pose.position
-            self.wm.X[0, 0] = self.wm.xhat_r_t0.point.x
-            self.wm.X[1, 0] = self.wm.xhat_r_t0.point.y
-            self.wm.X[2, 0] = self.wm.xhat_r_t0.point.z
+            
+        self.wm.X[0, 0] = self.wm.xhat_r_t0.point.x
+        self.wm.X[1, 0] = self.wm.xhat_r_t0.point.y
+        self.wm.X[2, 0] = self.wm.xhat_r_t0.point.z
             
         else:
             measurement = self.transform_pose(
