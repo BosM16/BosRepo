23edd661bd7f38ccb6958a45562411c5d0b6ee76
RianBeck <rian.beckd@gmail.com>
2018-11-01 15:07:38 +0100

    added continuous matrices

--- bebop_ws/src/bebop_demo/src/world_model.py
+++ bebop_ws/src/bebop_demo/src/world_model.py
@@ -46,8 +46,22 @@ class WorldModel(object):
         # State space model matrices for position Kalman filter in
         # continuous time!! Are then converted to discrete time further on
         # depending on Ts
-        self.A = np.identity(9)  # continuous A matrix
+        Ax = np.array([[0, 0, 0.05182], [1, 0, -2.65], [0, 1, -5.208]])
+        Ay = np.array([[0, 0, 0.08711], [1, 0, -3.781], [0, 1, -6.557]])
+        Az = np.array([[0, 0, 0.5799], [1, 0, -32.17], [0, 1, -9.168]])
+        self.A = np.zeros([9, 9])  # continuous A matrix
+        self.A[1:3, 1:3] = Ax
+        self.A[4:6, 4:6] = Ay
+        self.A[7:9, 7:9] = Az
+
+        Bx = np.array([[18.04], [-0.8863], [0.02308]])
+        By = np.array([[21.8], [-1.764], [0.02634]])
+        Bz = np.array([[30.68], [6.186], [-0.01141]])
         self.B = np.zeros([9, 3])  # continuous B matrix
+        self.B[1:3, 1] = np.transpose(Bx)
+        self.B[4:6, 2] = np.transpose(By)
+        self.B[7:9, 3] = np.transpose(Bz)
+
         self.C = np.zeros([3, 9])
         self.C[0, 0] = 1
         self.C[1, 3] = 1
@@ -69,14 +83,15 @@ class WorldModel(object):
                       [vel_cmd.linear.y],
                       [vel_cmd.linear.z]])
 
-        self.X = np.matmul(Ts*self.A + np.identity(9), self.X) +
-            np.matmul(Ts*self.B, u)
+        self.X = (np.matmul(Ts*self.A + np.identity(9), self.X)
+                  + np.matmul(Ts*self.B, u))
 
         self.xhat_r.point.x = self.X[0, 0]
         self.xhat_r.point.y = self.X[3, 0]
         self.xhat_r.point.z = self.X[6, 0]
 
-        self.Phat = np.matmul(self.A, np.matmul(self.Phat, np.transpose(self.A))) + self.Q
+        self.Phat = np.matmul(self.A, np.matmul(
+            self.Phat, np.transpose(self.A))) + self.Q
 
     def correct_pos_update(self, pos_meas):
         """
