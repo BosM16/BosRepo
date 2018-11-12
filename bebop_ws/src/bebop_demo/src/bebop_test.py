0aa65975e03d118800c04f22b275f7dca634390d
RianBeck <rian.beckd@gmail.com>
2018-11-12 09:41:35 +0100

    added working kalman filter

--- bebop_ws/src/bebop_demo/src/bebop_test.py
+++ bebop_ws/src/bebop_demo/src/bebop_test.py
@@ -4,6 +4,7 @@ from geometry_msgs.msg import Twist, PoseStamped, Point, PointStamped
 from std_msgs.msg import Empty
 from bebop_demo.srv import GetPoseEst, GetPoseEstResponse, GetPoseEstRequest
 import numpy as np
+import math as m
 import rospy
 import tf2_ros
 import tf2_geometry_msgs as tf2_geom
@@ -22,11 +23,12 @@ class Demo(object):
         Initialization of Demo object.
         '''
         self.init = True
+        self.case5 = False
 
         rospy.init_node('bebop_demo')
 
         self.vel_cmd_list = []
-        self.point_pub = PointStamped()
+        self.vel_list_corr = []
 
         self.tfBuffer = tf2_ros.Buffer()
         self.listener = tf2_ros.TransformListener(self.tfBuffer)
@@ -47,12 +49,12 @@ class Demo(object):
         '''
         Starts running of bebop_demo node.
         '''
-        print '-------------------- \n Kalman started \n --------------------'
+        print '-------------------- \n Demo started \n --------------------'
         rospy.spin()
 
     def vive_ready(self, *_):
         '''
-        Provide get pose service when vive is calibrated.
+        Provides get_pose service when vive is calibrated.
         '''
         self._get_pose_service = rospy.Service(
             "/world_model/get_pose", GetPoseEst, self.get_kalman_pos_est)
@@ -61,7 +63,7 @@ class Demo(object):
         '''
         Receives a time-stamped twist and returns an estimate of the position
         Argument:
-            - vel_cmd = TwistStamped
+            - req_vel = TwistStamped
         '''
         self.init = False
 
@@ -71,7 +73,8 @@ class Demo(object):
 
         # Publish latest estimate to read out Kalman result.
         # Transform xhat to world frame.
-        self.transform_point("world_rot", "world")
+        self.wm.xhat = self.transform_point(
+            self.wm.xhat_r, "world_rot", "world")
         # print "xhat\n", self.wm.xhat
         # print "xhat_r\n", self.wm.xhat_r
         self.pose_r_pub.publish(self.wm.xhat_r)
@@ -83,99 +86,202 @@ class Demo(object):
         '''
         Based on the velocity commands send out by the velocity controller,
         calculate a prediction of the position in the future.
-
         Arguments:
             vel_cmd: TwistStamped
         '''
+        print 'PREDICT STEP', '\n'
         if not self.init:
-            self.wm.predict_pos_update(vel_cmd, self.wm.vel_cmd_Ts)
-            print 'Kalman predict step'
-        # Results in an updated xhat_r.
+            (self.wm.X_r, self.wm.xhat_r) = self.wm.predict_pos_update(
+                vel_cmd, self.wm.vel_cmd_Ts, self.wm.X_r)
 
     def kalman_pos_correct(self, measurement_world):
         '''
         Whenever a new position measurement is available, sends this
         information to the perception and then triggers the kalman filter
         to apply a correction step.
-
+        Cases:
+            - case 1: multiple velocity commands between successive
+                      measurements
+            - case 2: only one velocity command between successive
+                      measurements
+            - case 3: no velocity command between successive measurements
+            - case 4: last velocity command corresponds to time after new
+                      measurement, but due to delay is already in the list.
+            - case 5: last velocity command before measurement is not yet in
+                      the list and appears only in the next correction step.
         Arguments:
             measurement_world: PoseStamped expressed in "world" frame.
         '''
         measurement = self.transform_pose(
                                 measurement_world, "world", "world_rot")
+        self.pc.pose_vive = measurement_world
         if self.init:
+            self.wm.xhat_r_t0.header = measurement.header
             self.wm.xhat_r_t0.point = measurement.pose.position
-
-        # set xhat equal to xhat at t0 to start prediction from here
-        self.wm.xhat_r = self.wm.xhat_r_t0
+            self.wm.X_r_t0[0, 0] = self.wm.xhat_r_t0.point.x
+            self.wm.X_r_t0[3, 0] = self.wm.xhat_r_t0.point.y
+            self.wm.X_r_t0[6, 0] = self.wm.xhat_r_t0.point.z
+            self.wm.xhat_r = self.wm.xhat_r_t0
+            self.wm.X_r = self.wm.X_r_t0
 
         if not self.init:
-            self.pc.pose_vive = measurement_world
-            print "Applying Kalman correction step"
+            self.vel_list_corr = self.vel_list_corr + self.vel_cmd_list
+            self.vel_cmd_list = []
+            # print 'CORRECT STEP,\n'
+            # print 'times of vel_cmds at start:\n'
+            # for vel in self.vel_list_corr:
+            #     t_vel = self.wm.get_timestamp(vel)
+            #     print '-  ', t_vel - m.floor(t_vel)
 
+            if (len(self.vel_list_corr) > 1) and self.wm.get_time_diff(
+                    self.pc.pose_vive, self.vel_list_corr[1]) < 0:
+                self.vel_list_corr = self.vel_list_corr[1:]
+            # print 'vel cmd _list length after mod', len(self.vel_list_corr)
+            self.case5 = False
+            # tpose = self.wm.get_timestamp(self.pc.pose_vive)
+            # txhatr = self.wm.get_timestamp(self.wm.xhat_r_t0)
+            # print 'tpose', tpose - m.floor(tpose)
+            # print 'txhatr', txhatr - m.floor(txhatr)
+            # print "Applying Kalman correction step"
+            #
             # print "measurement_world\n", measurement_world
             # print "measurement\n", measurement
 
             # First make prediction from old point t0 to last point t before
             # new measurement.
             # Calculate variable B (time between latest prediction and new t0).
-            t_last_update = self.wm.get_timestamp(self.latest_vel_cmd)
-            new_t0 = self.wm.get_timestamp(self.pc.pose_vive)
-            time_diff_check = new_t0 - t_last_update
+
+            time_diff_check = self.wm.get_time_diff(
+                self.pc.pose_vive, self.latest_vel_cmd)
             late_cmd_vel = []
             # Check for case 4.
+            # print 'len velocity list before shrinking', len(self.vel_list_corr)
             if time_diff_check < 0:
-                t_last_update = self.wm.get_timestamp(self.vel_cmd_list[-2])
-                late_cmd_vel = [self.vel_cmd_list[-1]]
-                self.vel_cmd_list = self.vel_cmd_list[0:-1]
+                # print 'time diff check NOT ok, adjust t_last_update'
+                # t_last_update = self.wm.get_timestamp(self.vel_list_corr[-2])
+                late_cmd_vel = [self.vel_list_corr[-1]]
+                self.vel_list_corr = self.vel_list_corr[0:-1]
 
-            vel_len = len(self.vel_cmd_list)
-
+            vel_len = len(self.vel_list_corr)
+            # print 'len velocity list before check for case 3', vel_len
             if (vel_len > 1):
-                tstamp = self.wm.get_timestamp(self.vel_cmd_list[1])
+                case3 = False
+                Ts = self.wm.get_time_diff(
+                    self.vel_list_corr[1], self.wm.xhat_r_t0)
+                tstamp = self.wm.get_timestamp(self.vel_list_corr[1])  # For debugging only!
+                # print 'case 1 or 2'
             else:
-                tstamp = new_t0
-            self.wm.predict_pos_update(
-                    self.vel_cmd_list[0], (tstamp - self.wm.t0))
+                case3 = True
+                Ts = self.wm.get_time_diff(
+                    self.pc.pose_vive, self.wm.xhat_r_t0)
+                tstamp = self.wm.get_timestamp(self.pc.pose_vive)  # For debugging only!
+            # print 'Ts 151\n', Ts
+
+            # t_1st_velcmd = self.wm.get_timestamp(self.vel_list_corr[0])  # For debugging only!
+            # t_last_update = self.wm.get_timestamp(self.vel_list_corr[-1])
+
+            # old_t0 = self.wm.get_timestamp(self.wm.xhat_r_t0)
+            # new_t0 = self.wm.get_timestamp(self.pc.pose_vive)
+            # if old_t0 == new_t0:
+            #     print '--------------\n equal\n---------------\n'
+
+            # Print all times of vel cmds:
+            # print 'times of vel_cmds:\n'
+            # for vel in self.vel_list_corr:
+            #     t_vel = self.wm.get_timestamp(vel)
+            #     print '-  ', t_vel - m.floor(t_vel)
+            # print 'tstamp of first vel cmd', t_1st_velcmd - m.floor(t_1st_velcmd, -2)
+            # print 'tstamp of second vel cmd', tstamp - m.floor(tstamp, -2)
+            # print 'old t0', old_t0 - m.floor(old_t0)
+            # print 'new t0', new_t0 - m.floor(new_t0)
+            # print 't_last_update', t_last_update - m.floor(t_last_update, -2)
+
+            (X, xhat_r) = self.wm.predict_pos_update(
+                    self.vel_list_corr[0], Ts, self.wm.X_r_t0)  # Ts should be made variable depending on time between vel cmd's.
             # If not case 2 or 3 -> need to predict up to
             # last vel cmd before new_t0
             if vel_len > 2:
+                # Case 1.
                 for i in range(vel_len - 2):
-                    self.wm.predict_pos_update(
-                        self.vel_cmd_list[i], self.wm.vel_cmd_Ts)
+                    Ts = self.wm.get_time_diff(
+                        self.vel_list_corr[i+2], self.vel_list_corr[i+1])
+                    (X, xhat_r) = self.wm.predict_pos_update(
+                        self.vel_list_corr[i+1], Ts, X)
 
             # Now make prediction up to new t0 if not case 3.
-            B = new_t0 - t_last_update
-            if not tstamp == new_t0:
-                self.wm.predict_pos_update(self.latest_vel_cmd, B)
+            B = self.wm.get_time_diff(self.pc.pose_vive, self.vel_list_corr[-1])
+            if B > self.wm.vel_cmd_Ts:
+                # print 'case 5'
+                self.case5 = True
+            # print 'Ts 164\n', B
+            if not case3:
+                # print 'not case 3'
+                (X, xhat_r) = self.wm.predict_pos_update(
+                    self.latest_vel_cmd, B, X)
+
+            # ---- CORRECTION ----
             # Correct the estimate at new t0 with the measurement.
-            self.wm.correct_pos_update(measurement)
+            (X, xhat_r) = self.wm.correct_pos_update(measurement, X, xhat_r)
+            self.wm.X_r_t0 = X
+            self.wm.xhat_r_t0 = xhat_r
+            self.wm.xhat_r_t0.header.stamp = measurement.header.stamp
+
             # Now predict until next point t that coincides with next timepoint
             # for the controller.
-            self.wm.predict_pos_update(
-                self.vel_cmd_list[-1], self.wm.vel_cmd_Ts - B)
+            # print 'Ts 183\n', (1 + case5)*self.wm.vel_cmd_Ts - B, '\n'
 
-            self.vel_cmd_list = [self.vel_cmd_list[-1]]
-            self.vel_cmd_list += late_cmd_vel
+            (X, xhat_r) = self.wm.predict_pos_update(
+                                    self.vel_list_corr[-1],
+                                    (1 + self.case5)*self.wm.vel_cmd_Ts - B, X)
 
-    def transform_point(self, _from, _to):
+            # Save variable globally
+            self.wm.xhat_r = xhat_r
+            self.wm.X_r = X
+
+            # self.pose_r_pub.publish(self.wm.xhat_r)
+            # self.pose_pub.publish(self.wm.xhat)
+
+            self.vel_list_corr = [self.vel_list_corr[-1]]
+            self.vel_list_corr += late_cmd_vel
+            # tfinished = rospy.Time.now().to_sec()
+            # print 'tfinished', tfinished - m.floor(tfinished), '\n ---- \n'
+            # print 'times of vel_cmds at end:\n'
+            # for vel in self.vel_list_corr:
+            #     t_vel = self.wm.get_timestamp(vel)
+            #     print '-  ', t_vel - m.floor(t_vel)
+            # print '\n ---- \n'
+
+
+
+    def transform_point(self, point, _from, _to):
         '''Transforms point (geometry_msgs/PointStamped) from frame "_from" to
         frame "_to".
+        Arguments:
+            - _from, _to = string, name of frame
         '''
         transform = self.get_transform(_from, _to)
-        self.wm.xhat = tf2_geom.do_transform_point(self.wm.xhat_r, transform)
+        point_transformed = tf2_geom.do_transform_point(point, transform)
+
+        return point_transformed
 
     def transform_pose(self, pose, _from, _to):
         '''Transforms pose (geometry_msgs/PoseStamped) from frame "_from" to
         frame "_to".
+        Arguments:
+            - _from, _to = string, name of frame
         '''
         transform = self.get_transform(_from, _to)
         pose_tf = tf2_geom.do_transform_pose(pose, transform)
+        pose_tf.header.stamp = pose.header.stamp
+        pose_tf.header.frame_id = _to
 
         return pose_tf
 
     def get_transform(self, _from, _to):
-        '''
+        '''Returns the TransformStamped msg of the transform from reference
+        frame '_from' to reference frame '_to'
+        Arguments:
+            - _from, _to = string, name of frame
         '''
         tf_f_in_t = self.tfBuffer.lookup_transform(
             _to, _from, rospy.Time(0), rospy.Duration(0.1))
--- bebop_ws/src/bebop_demo/src/world_model.py
+++ bebop_ws/src/bebop_demo/src/world_model.py
@@ -8,8 +8,12 @@ import tf2_ros
 
 class WorldModel(object):
     '''
-    Contains all information of the world: the position of the obstacles, and
-    the position and pose of the drone.
+    Contains all available information of the world:
+        - Drone dynamic model
+        - Position of the obstacles
+        - Position and pose of the drone
+    Executes predictions and corrections on the drone position estimate using
+    Kalman algorithm.
     '''
 
     def __init__(self):
@@ -18,61 +22,98 @@ class WorldModel(object):
         """
         # Parameters.
         self.vel_cmd_Ts = rospy.get_param('vel_cmd/sample_time', 0.01)  # s
-        self.max_vel = 0.4  # m/s
-        self.max_accel = 0.2  # m/s**2
-        self.t0 = rospy.get_time()
-        rospy.set_param('vel_cmd/max_vel', self.max_vel)
-        rospy.set_param('vel_cmd/max_accel', self.max_accel)
+        self.max_vel = rospy.get_param('motionplanner/vmax', 0.4)  # m/s
+        self.max_accel = rospy.get_param('motionplanner/amax', 0.2)  # m/s**2
+        self.drone_radius = rospy.get_param('motionplanner/drone_radius', 0.2)
 
+        # Kalman tuning parameters.
         self.R = np.identity(3)  # measurement noise covariance
-        self.Q = 1e-2*np.identity(9)  # process noise covariance
+        self.Q = 1e-1*np.identity(8)  # process noise covariance
 
         # Variables.
         self.xhat = PointStamped()
         self.xhat.header.frame_id = "world"
 
-        self.xhat_r_t0 = PointStamped()
-        self.xhat_r_t0.header.frame_id = "world_rot"
+        self.X_r = np.zeros(shape=(8, 1))
+        self.X_r_t0 = np.zeros(shape=(8, 1))
         self.xhat_r = PointStamped()
         self.xhat_r.header.frame_id = "world_rot"
+        self.xhat_r_t0 = PointStamped()
+        self.xhat_r_t0.header.frame_id = "world_rot"
 
-        self.Phat_t0 = np.zeros(9)
-        self.Phat = np.zeros(9)
-        self.X = np.zeros((9, 1))
-
-        # State space model matrices for position Kalman filter in
-        # continuous time!! Are then converted to discrete time further on
-        # depending on Ts
-        Ax = np.array([[0, 1, -5.208], [1, 0, -2.65], [0, 0, 0.05182]])
-        Ay = np.array([[0, 1, -6.557], [1, 0, -3.781], [0, 0, 0.08711]])
-        Az = np.array([[0, 1, -9.168], [1, 0, -32.17], [0, 0, 0.5799]])
-        self.A = np.zeros([9, 9])  # continuous A matrix
-        self.A[1:3, 1:3] = Ax
-        self.A[4:6, 4:6] = Ay
-        self.A[7:9, 7:9] = Az
-
-        Bx = np.array([[0.02308], [-0.8863], [18.04]])
-        By = np.array([[0.02634], [-1.764], [21.8]])
-        Bz = np.array([[-0.01141], [6.186], [30.68]])
-        self.B = np.zeros([9, 3])  # continuous B matrix
-        self.B[1:3, 1] = Bx
-        self.B[4:6, 2] = By
-        self.B[7:9, 3] = Bz
-
-        self.C = np.zeros([3, 9])
-        self.C[0, 0] = 1
-        self.C[1, 3] = 1
-        self.C[2, 6] = 1
+        self.Phat_t0 = np.zeros(8)
+        self.Phat = np.zeros(8)
 
         # Transformations.
         self.tfBuffer = tf2_ros.Buffer()
         self.listener = tf2_ros.TransformListener(self.tfBuffer)
 
-    def predict_pos_update(self, vel_cmd_stamped, Ts):
+        self.initialize_model()
+
+    def initialize_model(self):
+        '''Initializes The model to be used in the Kalman filter.
+        State space model x'(t) = A*x(t) + B*u(t) in observable canonical
+        form, corresponding to transfer function
+
+                  d1*s^2 + d2*s + d3
+        G(s) = --------------------------
+                s^3 + n1*s^2 + n2*s + n3
+
+        State space model matrices for position Kalman filter are in
+        continuous time!! Are then converted to discrete time further on
+        depending on varying Ts.
+        '''
+
+        a2x = 5.208
+        a1x = 2.65
+        a0x = -0.05182
+        Ax = np.array([[-a2x, 1., 0.],
+                       [-a1x, 0., 1.],
+                       [-a0x, 0., 0.]])
+        a2y = 6.557
+        a1y = 3.781
+        a0y = -0.08711
+        Ay = np.array([[-a2y, 1., 0.],
+                       [-a1y, 0., 1.],
+                       [-a0y, 0., 0.]])
+        a1z = 4.941
+        a0z = 0.01532
+        Az = np.array([[-a1z, 1.],
+                       [-a0z, 0.]])
+
+        self.A = np.zeros([8, 8])  # continuous A matrix
+        self.A[0:3, 0:3] = Ax
+        self.A[3:6, 3:6] = Ay
+        self.A[6:8, 6:8] = Az
+
+        b2x = 0.02308
+        b1x = -0.8863
+        b0x = 18.04
+        Bx = np.array([b2x, b1x, b0x])
+        b2y = 0.02634
+        b1y = -1.764
+        b0y = 21.8
+        By = np.array([b2y, b1y, b0y])
+        b1z = 0.09622
+        b0z = 4.886
+        Bz = np.array([b1z, b0z])
+        self.B = np.zeros([8, 3])  # continuous B matrix
+        self.B[0:3, 0] = Bx
+        self.B[3:6, 1] = By
+        self.B[6:8, 2] = Bz
+
+        self.C = np.zeros([3, 8])
+        self.C[0, 0] = 1
+        self.C[1, 3] = 1
+        self.C[2, 6] = 1
+
+    def predict_pos_update(self, vel_cmd_stamped, Ts, X):
         """
         Prediction step of the kalman filter. Update the position of the drone
         using the reference velocity commands.
-        vel_cmd_stamped = TwistStamped
+        Arguments:
+            - vel_cmd_stamped = TwistStamped
+            - Ts = varying step size over which to integrate.
         """
         vel_cmd = vel_cmd_stamped.twist
 
@@ -80,50 +121,61 @@ class WorldModel(object):
                       [vel_cmd.linear.y],
                       [vel_cmd.linear.z]])
 
-        self.X = (np.matmul(Ts*self.A + np.identity(9), self.X)
-                  + np.matmul(Ts*self.B, u))
+        X = (np.matmul(Ts*self.A + np.identity(8), X)
+             + np.matmul(Ts*self.B, u))
 
-        self.xhat_r.point.x = self.X[0, 0]
-        self.xhat_r.point.y = self.X[3, 0]
-        self.xhat_r.point.z = self.X[6, 0]
+        xhat_r = PointStamped()
+        xhat_r.header.frame_id = "world_rot"
+        xhat_r.point.x = X[0, 0]
+        xhat_r.point.y = X[3, 0]
+        xhat_r.point.z = X[6, 0]
 
-        self.Phat = np.matmul(self.A, np.matmul(
-            self.Phat, np.transpose(self.A))) + self.Q
+        self.Phat = np.matmul(Ts*self.A + np.identity(8), np.matmul(
+            self.Phat, np.transpose(Ts*self.A + np.identity(8)))) + self.Q
 
-    def correct_pos_update(self, pos_meas):
+        return X, xhat_r
+
+    def correct_pos_update(self, pos_meas, X, xhat_r):
         """
         Correction step of the kalman filter. Update the position of the drone
         using the measurements.
+        Argument:
+            - pos_meas = PoseStamped expressed in "world_rot" frame.
         """
-        self.X[0, 0] = self.xhat_r.point.x
-        self.X[3, 0] = self.xhat_r.point.y
-        self.X[6, 0] = self.xhat_r.point.z
-
         y = np.array([[pos_meas.pose.position.x],
                       [pos_meas.pose.position.y],
                       [pos_meas.pose.position.z]])
 
-        nu = y - np.matmul(self.C, self.X)
+        nu = y - np.matmul(self.C, X)
         S = np.matmul(self.C, np.matmul(
             self.Phat, np.transpose(self.C))) + self.R
         L = np.matmul(self.Phat, np.matmul(
             np.transpose(self.C), np.linalg.inv(S)))
-        self.X = self.X + np.matmul(L, nu)
+        X = X + np.matmul(L, nu)
         self.Phat = np.matmul(
-            (np.identity(9) - np.matmul(L, self.C)), self.Phat)
+            (np.identity(8) - np.matmul(L, self.C)), self.Phat)
 
-        self.xhat_r.point.x = self.X[0, 0]
-        self.xhat_r.point.y = self.X[3, 0]
-        self.xhat_r.point.z = self.X[6, 0]
+        xhat_r.point.x = X[0, 0]
+        xhat_r.point.y = X[3, 0]
+        xhat_r.point.z = X[6, 0]
 
-        self.xhat_r_t0 = self.xhat_r
-
-        # Time of last measurement is new t0.
-        self.t0 = self.get_timestamp(pos_meas)
+        return X, xhat_r
 
     def get_timestamp(self, stamped_var):
-        time = stamped_var.header.stamp.to_sec()
+        '''Returns the timestamp of 'stamped_var' (any stamped msg, eg.
+        PoseStamped, Pointstamped, TransformStamped,...) in seconds.
+        '''
+        time = float(stamped_var.header.stamp.to_sec())
+
         return time
+
+    def get_time_diff(self, stamp1, stamp2):
+        '''Returns the difference between to timestamped messages (any stamped
+        msg, eg. PoseStamped, Pointstamped, TransformStamped,...) in seconds.
+        '''
+        time_diff = self.get_timestamp(stamp1) - self.get_timestamp(stamp2)
+
+        return time_diff
 
 
 if __name__ == '__main__':
