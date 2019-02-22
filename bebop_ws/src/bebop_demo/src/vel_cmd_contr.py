9ca23a7e08cbcc65738b48b5d73a4e162c801c2e
BosMathias <bos.mathias16@gmail.com>
2019-02-22 16:34:05 +0100

    Crashing behavior at take-off is back...

--- bebop_ws/src/bebop_demo/src/vel_cmd_contr.py
+++ bebop_ws/src/bebop_demo/src/vel_cmd_contr.py
@@ -432,7 +432,7 @@ class VelCommander(object):
         self.hover_setpoint = self._goal
 
         while not self.target_reached:
-            if self.state_killed = True:
+            if self.state_killed == True:
                 break
 
             if self.startup:  # Becomes True when goal is set.
