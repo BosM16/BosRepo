5b5b784c91993427f00894d1b8c563592f31853c
RianBeck <rian.beckd@gmail.com>
2019-03-23 22:53:04 +0100

    fixed run_demo shell script

--- run_demo.sh
+++ run_demo.sh
@@ -1,16 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
-killall roscore
-
-cd bebop_ws/
-roscore &
-rviz &
-rqt &
-source devel/setup.bash
-roslaunch bebop_demo bebop_autonomy.launch &
-
-gnome-terminal --tab
-source devel/setup.bash
-roslaunch bebop_demo bebop_demo.launch
-
+gnome-terminal\
+ --tab -e "roscore"\
+ --tab -e 'sh -c "sleep 2; rviz"'\
+ --tab -e 'sh -c "sleep 2; rqt"'\
+ --tab -e 'sh -c "cd bebop_ws; source devel/setup.bash; sleep 2; roslaunch bebop_demo bebop_autonomy.launch"'\
+ --tab -e 'sh -c "cd bebop_ws; source devel/setup.bash; sleep 5; roslaunch bebop_demo bebop_demo.launch"'\
 
--- try.sh
+++ try.sh
@@ -1,6 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 killall roscore
+sleep 1
 
 cd bebop_ws/
 roscore &
@@ -12,20 +13,19 @@ echo tralala
 WID=$(xprop -root | grep "_NET_ACTIVE_WINDOW(WINDOW)"| awk '{print $5}')
 xdotool windowfocus $WID
 xdotool key ctrl+shift+t
-xdotool key alt+1
+xdotool key alt+2
 wmctrl -i -a $WID
 
-#source devel/setup.bash
-#roslaunch bebop_demo bebop_autonomy.launch
+source ~/Documents/BosRepo/bebop_ws/devel/setup.bash
+roslaunch bebop_demo bebop_autonomy.launch
 
 WID=$(xprop -root | grep "_NET_ACTIVE_WINDOW(WINDOW)"| awk '{print $5}')
 xdotool windowfocus $WID
 xdotool key ctrl+shift+t
-xdotool key alt+1
-#wmctrl -i -a $WID
+xdotool key alt+3
+wmctrl -i -a $WID
 echo joepie
 
-
-#source devel/setup.bash
-#gnome-terminal -e cd bebop_ws/ roslaunch bebop_demo bebop_demo.launch
+source devel/setup.bash
+#roslaunch bebop_demo bebop_demo.launch
 echo hoihoi
